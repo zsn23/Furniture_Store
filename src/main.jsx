19ad2ae533e490f523a9354132667b563f9a5f86
Muhammad Zeeshan <zeeshanijaz14@gmail.com>
2024-09-26 16:38:51 +0500

    new chnges

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-
-import { BrowserRouter } from 'react-router-dom'
-
+import { BrowserRouter } from 'react-router-dom';
+import { LanguageProvider } from './Pages/LanguageContext.jsx';
 createRoot(document.getElementById('root')).render(
-  
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
