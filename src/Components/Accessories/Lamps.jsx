a581aded44cf383c4e6b0ecb1f4db6ecef987500
HROON157 <haroonshafiq104@gmail.com>
2024-10-03 03:06:31 +0500

    new changes

--- src/Components/Accessories/Lamps.jsx
+++ src/Components/Accessories/Lamps.jsx
@@ -232,6 +232,229 @@ const Lamps = () => {
 export default Lamps;
 
 
+/*-----------------------------------------------Page Lamp Card zindex code------------------------------
+import React, { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import LampPic from "../Picture/LampImage.jpg";
+
+const Lamps = () => {
+  const [inStock, setInStock] = useState(false);
+  const [outOfStock, setOutOfStock] = useState(false);
+  const [showFilter, setShowFilter] = useState(false);
+  const [showPrice, setShowPrice] = useState(false);
+  const [fromPrice, setFromPrice] = useState('');
+  const [toPrice, setToPrice] = useState('');
+  const [sortOption, setSortOption] = useState('Relevance');
+  const [filterPosition, setFilterPosition] = useState({ top: 0, left: 0 });
+  const [pricePosition, setPricePosition] = useState({ top: 0, left: 0 });
+
+  const filterRef = useRef(null);
+  const priceRef = useRef(null);
+  const filterButtonRef = useRef(null);
+  const priceButtonRef = useRef(null);
+
+  const toggleFilterCard = () => {
+    const buttonRect = filterButtonRef.current.getBoundingClientRect();
+    setFilterPosition({ top: buttonRect.bottom + window.scrollY, left: buttonRect.left });
+    setShowFilter((prev) => !prev);
+    setShowPrice(false);
+  };
+
+  const togglePrice = () => {
+    const buttonRect = priceButtonRef.current.getBoundingClientRect();
+    setPricePosition({ top: buttonRect.bottom + window.scrollY, left: buttonRect.left });
+    setShowPrice((prev) => !prev);
+    setShowFilter(false);
+  };
+
+  const resetFilters = () => {
+    setInStock(false);
+    setOutOfStock(false);
+  };
+
+  const resetPrice = () => {
+    setFromPrice('');
+    setToPrice('');
+  };
+
+  const inputStyle = {
+    padding: '10px',
+    borderRadius: '7px',
+    border: '1px solid #000',
+    fontSize: '16px',
+    width: '100px',
+    outline: 'none'
+  };
+
+  const handleSortChange = (e) => {
+    setSortOption(e.target.value);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (filterRef.current && !filterRef.current.contains(event.target) && !filterButtonRef.current.contains(event.target)) {
+        setShowFilter(false);
+      }
+      if (priceRef.current && !priceRef.current.contains(event.target) && !priceButtonRef.current.contains(event.target)) {
+        setShowPrice(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="container">
+      <p className="mt-4">
+        <Link
+          to="/"
+          className="text-decoration-none fs-5 text-muted me-2 breadcrumb-link"
+        >
+          Home &gt;
+        </Link>
+        <Link
+          to="/"
+          className="text-decoration-none fs-5 text-muted me-2 breadcrumb-link"
+        >
+          Accessories &gt;
+        </Link>
+        <Link
+          to="/accessories/lamps"
+          className="text-decoration-none fs-5 breadcrumb-link"
+          style={{ color: "#84754E", fontWeight: "bold" }}
+        >
+          Lamps
+        </Link>
+      </p>
+      <h1 className="fontStyling mt-4">Lamps</h1>
+
+      <div className="d-flex align-items-center justify-content-between mt-5 flex-wrap" style={{ position: 'relative', overflow: 'hidden' }}>
+        <div className="d-flex align-items-center mb-2">
+          <p className="m-0 p-0 text-muted me-2">Filter: </p>
+          <p
+            className="m-0 p-0 underline_Availabilty text-muted me-3"
+            onClick={toggleFilterCard}
+            ref={filterButtonRef}
+            style={{ cursor: "pointer" }}
+          >
+            Availability <i className="bi bi-caret-down-fill caret"></i>
+          </p>
+          <p
+            className="m-0 p-0 underline_Availabilty text-muted"
+            style={{ cursor: "pointer" }}
+            onClick={togglePrice}
+            ref={priceButtonRef}
+          >
+            Price <i className="bi bi-caret-down-fill caret"></i>
+          </p>
+        </div>
+
+        <div className="d-flex align-items-center">
+          <p className="p-0 m-0 text-muted me-2">Sort by:</p>
+          <select
+            className="form-select custom_input text-muted"
+            value={sortOption}
+            onChange={handleSortChange}
+            style={{ width: '180px' }}
+          >
+            <option value="Relevance">Relevance</option>
+            <option value="PriceLowToHigh">Price: Low to High</option>
+            <option value="PriceHighToLow">Price: High to Low</option>
+            <option value="Newest">Newest Arrivals</option>
+          </select>
+        </div>
+      </div>
+
+      {showFilter && (
+        <div ref={filterRef} className="card mt-3 border-black" style={{ position: "absolute", top: filterPosition.top, left: filterPosition.left, width: "300px", zIndex: 1000 }}>
+          <div className="card-body p-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <p className="card-title m-0 text-muted">0 selected</p>
+              <button
+                className="btn p-0 underline_Availabilty text-muted"
+                onClick={resetFilters}
+                style={{ color: "black", border: "none", background: "none" }}
+              >
+                Reset
+              </button>
+            </div>
+            <hr style={{ border: "1px solid black", width: "100%" }} />
+            <div className="form-check mt-2">
+              <input
+                type="checkbox"
+                className="form-check-input custom_input"
+                checked={inStock}
+                onChange={() => setInStock(!inStock)}
+              />
+              <label className="form-check-label text-muted">
+                In stock (9)
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input custom_input"
+                checked={outOfStock}
+                onChange={() => setOutOfStock(!outOfStock)}
+              />
+              <label className="form-check-label text-muted">
+                Out of stock (2)
+              </label>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showPrice && (
+        <div ref={priceRef} className="card mt-3 border-black" style={{ position: "absolute", top: pricePosition.top, left: pricePosition.left, width: "315px", zIndex: 1000 }}>
+          <div className="card-body p-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <p className="card-title m-0 text-muted me-5">
+                The highest price is Rs.3600
+              </p>
+              <button
+                className="btn p-0 underline_Availabilty text-muted"
+                onClick={resetPrice}
+                style={{ color: "black", border: "none", background: "none" }}
+              >
+                Reset
+              </button>
+            </div>
+            <hr style={{ border: "1px solid black", width: "100%" }} />
+            <div style={{ display: 'flex', gap: '20px' }}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <span className="me-1">Rs</span>
+                <input
+                  type="number"
+                  placeholder="From"
+                  value={fromPrice}
+                  onChange={(e) => setFromPrice(e.target.value)}
+                  style={inputStyle}
+                />
+              </div>
+
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <span className="me-1">Rs</span>
+                <input
+                  type="number"
+                  placeholder="To"
+                  value={toPrice}
+                  onChange={(e) => setToPrice(e.target.value)}
+                  style={inputStyle}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Lamps;
+ */
 
 
 /*
