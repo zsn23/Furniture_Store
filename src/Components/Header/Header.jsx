199ccf9993df9510dd99fa808c53d3e456736eec
HROON157 <haroonshafiq104@gmail.com>
2024-10-04 01:25:12 +0500

    many chenges

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -304,7 +304,8 @@ switch (language) {
     {/* FURNITURE HOP Logo */}
     <div className="col-12 col-md-6 d-flex justify-content-md-start justify-content-center mb-3 mb-md-0">
       <Link to='/' className='text-decoration-none'>
-        <h2 className="fontStyling mt-3">FURNITURE HOP</h2>
+      <h2 className="fontStyling mt-3 text-center text-sm-end HandleHeading">FURNITURE HOP</h2>
+
       </Link>
     </div>
 
@@ -312,16 +313,17 @@ switch (language) {
     <div className="col-12 col-md-6 col-lg-6 d-flex justify-content-end">
       <div className="d-flex flex-wrap align-items-center">
         {/* Location */}
-        <div className="d-flex align-items-center px-2">
-          <small className="d-flex align-items-center">
-            <Link to='/location' className='text-decoration-none LocationMyaccountHover_____'>
-              <svg xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 -960 960 960" width="21px" fill="#000000">
-                <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
-              </svg>
-              Location |
-            </Link>
-          </small>
-        </div>
+        <div className="d-flex align-items-center px-0">
+  <small className="d-flex align-items-center LocationMyaccountHover_____">
+    <Link to='/location' className='text-decoration-none'>
+      <svg xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 -960 960 960" width="21px" fill="#000000">
+        <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
+      </svg>
+      <span className="LocationLogo"> <small> Location |</small></span>
+    </Link>
+  </small>
+</div>
+
 
         {/* My Account */}
         <div className="d-flex align-items-center px-2">
@@ -334,8 +336,8 @@ switch (language) {
               <svg xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 -960 960 960" width="22px" fill="#000000">
                 <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
               </svg>
-              My Account |
-              {accountHovered && (
+              <span className="LocationLogo"> <small> My Account |</small></span>
+                {accountHovered && (
                 <div className="dropdown-menu show account-dropdown">
                   <ul>
                     <li><Link to="/login" className="dropdown-item hover-underline">Sign In or Create Account</Link></li>
@@ -349,9 +351,9 @@ switch (language) {
 
         {/* My Cart */}
         <div className="d-flex align-items-center px-2">
-          <Link to='/MyCart' className='text-decoration-none'>
-            <small className="d-flex justify-content-center align-items-center MycartBtn  text-dark px-1 py-1 border border-dark rounded-pill hover-underline">
-              MY CART
+          <Link to='/MyCart' className='text-decoration-none '>
+            <small className="d-flex justify-content-center align-items-center MycartBtn  text-dark px-2 py-1 border border-dark rounded-pill hover-underline ">
+             <small> MY CART</small>
               {cartTotalQuantity > 0 && (
                 <span className="cart-counter">{cartTotalQuantity}</span>
               )}
@@ -360,10 +362,10 @@ switch (language) {
         </div>
 
         {/* My Wishlist */}
-        <div className="d-flex align-items-center px-2">
+        <div className="d-flex align-items-center px-0">
           <Link to='/Whishlist' className='text-decoration-none'>
-            <small className="d-flex justify-content-center align-items-center MycartBtn text-dark px-1 py-1 border border-dark rounded-pill hover-underline">
-              My Wishlist
+            <small className="d-flex justify-content-center align-items-center MycartBtn text-dark px-2 py-1 border border-dark rounded-pill hover-underline ">
+            <small>  My Wishlist</small>
             </small>
           </Link>
         </div>
@@ -374,9 +376,9 @@ switch (language) {
       {/* Row for Navigation Links and Search */}
       <div className="row align-items-center">
         {/* Country Dropdown */}
-        <div className="col-12 col-md-6 d-flex justify-content-md-start justify-content-center">
+        <div className="col-12 col-md-6 d-flex justify-content-md-start">
           <div 
-            className="dropdown"
+            className="dropdown "
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
           >
